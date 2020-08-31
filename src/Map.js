b6c1989ba23a5f52637edaa644a3f1624f0ee9eb
austinb847 <austinb847@icloud.com>
2020-08-30 18:01:48 -0700

    Add starting location to map on geocoder result

--- src/Map.js
+++ src/Map.js
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import './Map.css';
 import {useState, useRef} from 'react';
-import MapGL from 'react-map-gl';
+import MapGL, { Layer, Marker} from 'react-map-gl';
+// import {Feature, Marker} from 'react-mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css'
 import Geocoder from "react-map-gl-geocoder";
 import "react-map-gl-geocoder/dist/mapbox-gl-geocoder.css";
 import Sidebar from './Sidebar';
+import * as turf from '@turf/turf'
 
 function Map() {
   const [viewport, setViewport] = useState({
@@ -14,6 +16,13 @@ function Map() {
     zoom: 3
   });
   const mapRef = useRef();
+  const [startlocation, setStartLocation] = useState();
+
+
+  const createStartLocation = () => {
+    console.log(startlocation[1])
+    console.log(startlocation[0])
+  }
 
   return (
     <MapGL
@@ -21,11 +30,16 @@ function Map() {
       ref={mapRef}
       width="100vw"
       height="100vh"
-      mapStyle="mapbox://styles/mapbox/dark-v9"
+      mapStyle='mapbox://styles/mapbox/streets-v11'
       onViewportChange={nextViewport => setViewport(nextViewport)}
       mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
     >
       <Sidebar/>
+
+      {startlocation &&
+        <Marker className='start-location'latitude={startlocation[1]} longitude={startlocation[0]} offsetLeft={-20} offsetTop={-10}>
+      </Marker>
+      }
       <Geocoder
         {...viewport}
         mapRef={mapRef}
@@ -33,6 +47,8 @@ function Map() {
         mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
         position="top-right"
         countries='us'
+        limit={10}
+        onResult={e => setStartLocation(e.result.geometry.coordinates)}
       />
     </MapGL>
   );
