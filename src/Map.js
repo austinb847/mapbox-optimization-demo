69f88c60cf231a06b984734da52ab05eb2151673
austinb847 <austinb847@icloud.com>
2020-08-30 23:41:29 -0700

    handle button clicks. Save state of clicked button values

--- src/Map.js
+++ src/Map.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import './Map.css';
 import {useState, useRef} from 'react';
-import MapGL, { Layer, Marker} from 'react-map-gl';
+import MapGL, {Marker} from 'react-map-gl';
 // import {Feature, Marker} from 'react-mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css'
 import Geocoder from "react-map-gl-geocoder";
@@ -10,47 +10,49 @@ import Sidebar from './Sidebar';
 import * as turf from '@turf/turf'
 
 function Map() {
+  const mapRef = useRef();
   const [viewport, setViewport] = useState({
     latitude: 37.0902,
     longitude: -95.7129,
     zoom: 3
   });
-  const mapRef = useRef();
   const [startlocation, setStartLocation] = useState();
-
-
-  const createStartLocation = () => {
-    console.log(startlocation[1])
-    console.log(startlocation[0])
-  }
-
+  const [destinations, setDestinations] = useState(turf.featureCollection([]));
+  const [routes, setRoutes] = useState(turf.featureCollection([]));
+  const [clickLocation, setClickLocation] = useState();
+  
   return (
-    <MapGL
-      {...viewport}
-      ref={mapRef}
-      width="100vw"
-      height="100vh"
-      mapStyle='mapbox://styles/mapbox/streets-v11'
-      onViewportChange={nextViewport => setViewport(nextViewport)}
-      mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
-    >
-      <Sidebar/>
-
-      {startlocation &&
-        <Marker className='start-location'latitude={startlocation[1]} longitude={startlocation[0]} offsetLeft={-20} offsetTop={-10}>
-      </Marker>
-      }
-      <Geocoder
+    <React.Fragment>
+      <MapGL
         {...viewport}
-        mapRef={mapRef}
+        ref={mapRef}
+        width="100vw"
+        height="100vh"
+        mapStyle='mapbox://styles/mapbox/streets-v11'
         onViewportChange={nextViewport => setViewport(nextViewport)}
         mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
-        position="top-right"
-        countries='us'
-        limit={10}
-        onResult={e => setStartLocation(e.result.geometry.coordinates)}
-      />
-    </MapGL>
+      >
+
+        {startlocation &&
+          <Marker className='start-location'
+            latitude={startlocation[1]} 
+            longitude={startlocation[0]}>
+          </Marker>
+        }
+        <Geocoder
+          // {...viewport}
+          mapRef={mapRef}
+          onViewportChange={nextViewport => setViewport(nextViewport)}
+          mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
+          placeholder="Enter Location Here"
+          position="top-right"
+          countries='us'
+          zoom={6}
+          onResult={e => setStartLocation(e.result.geometry.coordinates)}
+        />
+      </MapGL>
+      <Sidebar applyDestinations={setDestinations} handleClick={setClickLocation}/>
+    </React.Fragment>
   );
 }
 
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import { Button, Icon } from 'semantic-ui-react'
 
-function Sidebar() {
+function Sidebar(props) {
+  const {applyDestinations, handleClick} = props
 
-
-
+  
 
 
 
@@ -14,37 +14,41 @@ function Sidebar() {
     <div className='info-box'>
           <h1>Trip Optimizer</h1>
           <p>Click to add Locations to visit:</p>
-          <Button icon labelPosition='left' value='-87.675171,42.055984' name='college' onClick={(e) => this.addLocation(e, 'value')}>  
-            <Icon name='graduation cap' />
-            Northwestern University
+          <Button icon labelPosition='left' 
+            value='-87.675171,42.055984' 
+            name='college' 
+            onClick={(e) => handleClick({coordinates: e.target.value.split(',').map(e => parseFloat(e)), btnClicked: e.target.name})}
+            >  
+              <Icon name='graduation cap' />
+              Northwestern University
           </Button>
           <br/>
           <br/>
-          <Button icon labelPosition='left' value='-87.681546, 42.046177' name='restaurant' onClick={(e) => this.addLocation(e, 'value')}>
+          <Button icon labelPosition='left' value='-87.681546, 42.046177' name='restaurant' onClick={(e) => handleClick({coordinates: e.target.value.split(',').map(e => parseFloat(e)), btnClicked: e.target.name})}>
             <Icon name='utensils' />
             Restaurant
           </Button>
           <br/>
           <br/>
-          <Button icon labelPosition='left' value='-87.680730, 42.048753' name='hotel' onClick={(e) => this.addLocation(e, 'value')}>
+          <Button icon labelPosition='left' value='-87.680730, 42.048753' name='hotel' onClick={(e) => handleClick({coordinates: e.target.value.split(',').map(e => parseFloat(e)), btnClicked: e.target.name})}>
             <Icon name='hotel' />
             Hotel
           </Button>
           <br/>
           <br/>
-          <Button icon labelPosition='left' value='-86.5126,39.1754' name='college' onClick={(e) => this.addLocation(e, 'value')}>  
+          <Button icon labelPosition='left' value='-86.5126,39.1754' name='college' onClick={(e) => handleClick({coordinates: e.target.value.split(',').map(e => parseFloat(e)), btnClicked: e.target.name})}>  
             <Icon name='graduation cap' />
             Indiana University
           </Button>
           <br/>
           <br/>
-          <Button icon labelPosition='left' value='-86.529617,39.167554' name='restaurant' onClick={(e) => this.addLocation(e, 'value')}>
+          <Button icon labelPosition='left' value='-86.529617,39.167554' name='restaurant' onClick={(e) => handleClick({coordinates: e.target.value.split(',').map(e => parseFloat(e)), btnClicked: e.target.name})}>
             <Icon name='utensils' />
             Restaurant
           </Button>
           <br/>
           <br/>
-          <Button icon labelPosition='left' value='-86.535488,39.170568' name='hotel' onClick={(e) => this.addLocation(e, 'value')}>
+          <Button icon labelPosition='left' value='-86.535488,39.170568' name='hotel' onClick={(e) => handleClick({coordinates: e.target.value.split(',').map(e => parseFloat(e)), btnClicked: e.target.name})}>
             <Icon name='hotel' />
             Hotel
           </Button>
